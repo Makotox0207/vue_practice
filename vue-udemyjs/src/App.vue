110039ed0c6b21b833e2be61064ce880cc6f6ba8
Makotox0207 <xiaoxishicheng@gmail.com>
2021-06-05 17:43:57 +0900

    フック関数

--- vue-udemyjs/src/App.vue
+++ vue-udemyjs/src/App.vue
@@ -2,21 +2,12 @@
   <div style="padding: 10rem;">
     <button @click="currentComponent = 'Home'">Home</button>
     <button @click="currentComponent = 'About'">About</button>
-    <!-- 動的にAboutまたはHomeに変更 -->
     <component :is="currentComponent"></component>
-    <!-- 一緒 -->
     <About v-if="currentComponent === 'About'"></About>
     <Home v-if="currentComponent === 'Home'"></Home>
-    <!-- keep-aliveをつけると全てのデータがキャッシュされる -->
     <keep-alive>
       <component :is="currentComponent"></component>
     </keep-alive>
-
-    <!-- $eventで$emitのイベント(第2引数)を受け取る -->
-    <!-- <EventTitle
-      :value="formData.title"
-      @input="formData.title = $event">
-    </EventTitle> -->
     <EventTitle v-model="formData.title"></EventTitle>
   </div>
 </template>
