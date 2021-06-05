941e5ad0d02d60da83f9c9a3b18c312a9a4c9640
Makotox0207 <xiaoxishicheng@gmail.com>
2021-06-05 20:00:06 +0900

    ローカルに登録

--- vue-udemyjs/src/components/Home.vue
+++ vue-udemyjs/src/components/Home.vue
@@ -3,3 +3,27 @@
   <!-- 引数はv-directiveName:引数 -->
   <p v-border:solid.round.shadow="{width: '5px', color: 'red'}">home</p>
 </template>
+
+<script>
+  export default{
+    // directives: {} ローカルにディレクティブを追加する
+    directives:  {
+      border(el, binding){
+        // elはhtml要素を指定する (pタグを指定)
+        el.style.border = "solid black 2px";
+        // binding.value.key 複数の値を受け取る
+        el.style.borderWidth = binding.value.width;
+        el.style.borderColor = binding.value.color;
+        // binding.arg 引数を取る ※argument=引数
+        el.style.borderStyle = binding.arg;
+        // if(binding.modiriers.修飾子){} if文で適応させる ※modifier=修飾子
+        if (binding.modifiers.round) {
+          el.style.borderRadius = "0.5rem";
+        }
+        if (binding.modifiers.shadow) {
+          el.style.boxShadow = "0.2px 5px rgba(0, 0, 0, 0.26";
+        }
+      } 
+    }
+  }
+</script>
--- vue-udemyjs/src/main.js
+++ vue-udemyjs/src/main.js
@@ -3,22 +3,22 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 // bindとupdateは基本的に同じ関数になるので一つにまとめて省略できる
-Vue.directive("border", function(el, binding) {
-  // elはhtml要素を指定する (pタグを指定)
-  el.style.border = "solid black 2px";
-  // binding.value.key 複数の値を受け取る
-  el.style.borderWidth = binding.value.width;
-  el.style.borderColor = binding.value.color;
-  // binding.arg 引数を取る ※argument=引数
-  el.style.borderStyle = binding.arg;
-  // if(binding.modiriers.修飾子){} if文で適応させる ※modifier=修飾子
-  if (binding.modifiers.round) {
-    el.style.borderRadius = "0.5rem";
-  }
-  if (binding.modifiers.shadow) {
-    el.style.boxShadow = "0.2px 5px rgba(0, 0, 0, 0.26";
-  }
-})
+// Vue.directive("border", function(el, binding) {
+//   // elはhtml要素を指定する (pタグを指定)
+//   el.style.border = "solid black 2px";
+//   // binding.value.key 複数の値を受け取る
+//   el.style.borderWidth = binding.value.width;
+//   el.style.borderColor = binding.value.color;
+//   // binding.arg 引数を取る ※argument=引数
+//   el.style.borderStyle = binding.arg;
+//   // if(binding.modiriers.修飾子){} if文で適応させる ※modifier=修飾子
+//   if (binding.modifiers.round) {
+//     el.style.borderRadius = "0.5rem";
+//   }
+//   if (binding.modifiers.shadow) {
+//     el.style.boxShadow = "0.2px 5px rgba(0, 0, 0, 0.26";
+//   }
+// })
 
 new Vue({
   render: h => h(App),
