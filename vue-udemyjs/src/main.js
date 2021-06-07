bd9f6bf8ed1e692b03be5fba2f15b845262783b4
Makotox0207 <xiaoxishicheng@gmail.com>
2021-06-07 17:57:17 +0900

    beforeEach

--- vue-udemyjs/src/main.js
+++ vue-udemyjs/src/main.js
@@ -7,6 +7,14 @@ Vue.filter("upperCase", function(value){
   return value.toUpperCase();
 });
 
+// ルートオブジェクト 現在のルートの状態を表すもの
+// to, from はルートオブジェクトが入る
+// next リダイレクト先を表す, これがないとずっと画面が真っ白, ページを遷移させなくするにはfalse, pathを指定して遷移させる
+router.beforeEach( (to, from, next) => {
+  if(to.path === '/users/1'){
+    next('/');
+  }
+})
 new Vue({
   router: router,
   render: h => h(App),
