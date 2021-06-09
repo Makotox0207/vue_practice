1772309133d859d66690bd6b6d2d6960a0f4b884
Makotox0207 <xiaoxishicheng@gmail.com>
2021-06-09 20:22:55 +0900

    mapMutaionsヘルパーで、効率よくmutationsをコンポーネントに追加する

--- vue_memo/vuex/mapMutaionsヘルパーで、効率よくmutationsをコンポーネントに追加する.md
+++ vue_memo/vuex/mapMutaionsヘルパーで、効率よくmutationsをコンポーネントに追加する.md
@@ -33,3 +33,33 @@ export default {
 
 ```
 
+## mapActionsヘルパーを使って、効率よくActionsをコンポーネントで使用する
+
+```html
+<template>
+  <div>
+    <h3>Home</h3>
+    <button @click="toUsers">Usersのページに行く</button>
+    <hr>
+    <h3>vuexの練習</h3>
+    <button @click="increment(1)">増加</button>
+    <button @click="decrement(1)">減少</button>
+  </div>
+</template>
+
+<script>
+import { mapActions } from 'vuex';
+export default {
+  methods: {
+    ...mapActions(['increment', 'decrement']),
+    // actionsのincrementをdispatchで実行する
+    // increment(){
+    //   this.$store.dispatch('increment', 1);
+    // },
+    // decrement(){
+    //   this.$store.dispatch('decrement', 1);
+    // },
+  }
+}
+```
+
--- vuex_practice/src/views/Home.vue
+++ vuex_practice/src/views/Home.vue
@@ -4,21 +4,23 @@
     <button @click="toUsers">Usersのページに行く</button>
     <hr>
     <h3>vuexの練習</h3>
-    <button @click="increment">増加</button>
-    <button @click="decrement">減少</button>
+    <button @click="increment(1)">増加</button>
+    <button @click="decrement(1)">減少</button>
   </div>
 </template>
 
 <script>
+import { mapActions } from 'vuex';
 export default {
   methods: {
+    ...mapActions(['increment', 'decrement']),
     // actionsのincrementをdispatchで実行する
-    increment(){
-      this.$store.dispatch('increment', 1);
-    },
-    decrement(){
-      this.$store.dispatch('decrement', 1);
-    },
+    // increment(){
+    //   this.$store.dispatch('increment', 1);
+    // },
+    // decrement(){
+    //   this.$store.dispatch('decrement', 1);
+    // },
     toUsers(){
       this.$router.push({
         name: 'users'
